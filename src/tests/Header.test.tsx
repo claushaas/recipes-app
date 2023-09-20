e248e74f64da2e6243963776f287aa8c0a93d396
Claus Haas <claus.haas@me.com>
2023-09-20 17:42:02 -0300

    requisito 8 parcial

---
+++ src/tests/Header.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import App from '../App';
+
+describe('Teste do componente Header', () => {
+  test('Se o Header Aparece na tela', () => {});
+});
---
+++ src/tests/helpers/renderWithReduxAndRouter.ts
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { applyMiddleware, legacy_createStore as createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from '../../redux/reducers/index'
+import thunk from 'redux-thunk';
+import { ReduxState } from '../../types';
+
+function renderWithRouterAndRedux(
+  component: JSX.Element,
+  route: string = '/',
+  state: ReduxState | undefined = undefined,
+  store = createStore(rootReducer, state, applyMiddleware(thunk))
+) {
+  window.history.pushState({}, 'Test page', route);
+
+  return {
+    ...render(
+      <BrowserRouter>
+        <Provider store={store}>{component}</Provider>
+      </BrowserRouter>
+    ),
+    user: userEvent.setup(),
+    store,
+  };
+}
