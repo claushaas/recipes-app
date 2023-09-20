7eeb9c8dca5bc4dfbc5c858097750b17a2637b51
Claus Haas <claus.haas@me.com>
2023-09-20 20:15:02 -0300

    req 9 done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,8 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
+import SearchBar from './SearchBar';
 
 type HeaderProps = {
   title: string;
@@ -9,17 +11,39 @@ type HeaderProps = {
 };
 
 function Header({ title, showSearch, showProfile }: HeaderProps) {
+  const [showSearchBar, setShowSearchBar] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSearchClick = () => {
+    setShowSearchBar(!showSearchBar);
+  };
+
+  const handleProfileClick = () => {
+    navigate('/profile');
+  };
+
   return (
     <header>
       <h1 data-testid="page-title">{title}</h1>
       {showSearch && (
-        <img src={ searchIcon } alt="Pesquisar" data-testid="search-top-btn" />
+        <button onClick={ handleSearchClick }>
+          <img
+            src={ searchIcon }
+            alt="Pesquisar"
+            data-testid="search-top-btn"
+          />
+        </button>
       )}
       {showProfile && (
-        <Link to="/profile">
-          <img src={ profileIcon } alt="Perfil" data-testid="profile-top-btn" />
-        </Link>
+        <button onClick={ handleProfileClick }>
+          <img
+            src={ profileIcon }
+            alt="Perfil"
+            data-testid="profile-top-btn"
+          />
+        </button>
       )}
+      {showSearchBar && <SearchBar />}
     </header>
   );
 }
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,9 @@
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" name="term" id="term" data-testid="search-input" />
+    </form>
+  );
+}
+
+export default SearchBar;
