146b7467f86c76cf319e42253c9d80e12ea7aa37
Claus Haas <claus.haas@me.com>
2023-09-20 15:53:52 -0300

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,32 @@
-import React from 'react';
 import './App.css';
-import rockGlass from './images/rockGlass.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Meals from './pages/Meals';
+import Drinks from './pages/Drinks';
+import MealDetails from './pages/MealDetails';
+import DrinkDetails from './pages/DrinkDetails';
+import MealProgress from './pages/MealProgress';
+import DrinkProgress from './pages/DrinkProgress';
+import Profile from './pages/Profile';
+import DoneRecipies from './pages/DoneRecipies';
+import FavoriteRecipies from './pages/FavoriteRecipies';
+// import rockGlass from './images/rockGlass.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="meals">
-      <span className="logo">TRYBE</span>
-      <object
-        className="rocksGlass"
-        type="image/svg+xml"
-        data={ rockGlass }
-      >
-        Glass
-      </object>
-    </div>
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+      <Route path="/meals" element={ <Meals /> } />
+      <Route path="/drinks" element={ <Drinks /> } />
+      <Route path="/meals/:id-da-receita" element={ <MealDetails /> } />
+      <Route path="/drinks/:id-da-receita" element={ <DrinkDetails /> } />
+      <Route path="/meals/:id-da-receita/in-progress" element={ <MealProgress /> } />
+      <Route path="/drinks/:id-da-receita/in-progress" element={ <DrinkProgress /> } />
+      <Route path="/profile" element={ <Profile /> } />
+      <Route path="/done-recipes" element={ <DoneRecipies /> } />
+      <Route path="favorite-recipes" element={ <FavoriteRecipies /> } />
+    </Routes>
   );
 }
 
