338db933f3b7d5095996bffb93fa2872806a08e7
DevNalther <nalther.l.b.ganem@gmail.com>
2023-09-20 16:09:28 -0300

    req 2 and 3

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,16 @@
 function Login() {
   return (
-    <div>Login</div>
+    <form>
+      <input
+        placeholder="E-Mail"
+        data-testid="email-input"
+      />
+      <input
+        placeholder="Password"
+        data-testid="password-input"
+      />
+      <button data-testid="login-submit-btn">Enter</button>
+    </form>
   );
 }
 
