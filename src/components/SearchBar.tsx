9e6c86c9743429eb351f1d119607533360e85f68
Claus Haas <claus.haas@me.com>
2023-09-21 07:24:35 -0300

    req 10

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,41 @@
+// import { useLocation } from 'react-router-dom';
+
 function SearchBar() {
+  // const location = useLocation();
+  // console.log(location.pathname);
   return (
     <form>
       <input type="text" name="term" id="term" data-testid="search-input" />
+      <label htmlFor="ingredient">
+        <input
+          data-testid="ingredient-search-radio"
+          id="ingredient"
+          name="ingredient"
+          type="radio"
+        />
+        Ingredient
+      </label>
+      <label htmlFor="name">
+        <input
+          data-testid="name-search-radio"
+          id="name"
+          name="name"
+          type="radio"
+        />
+        Name
+      </label>
+      <label htmlFor="firstLetter">
+        <input
+          data-testid="first-letter-search-radio"
+          id="firstLetter"
+          name="firstLetter"
+          type="radio"
+        />
+        First Letter
+      </label>
+      <button type="submit" data-testid="exec-search-btn">
+        Search
+      </button>
     </form>
   );
 }
