d11aab1429c2b9d58766324fdfad1d2eae914fdf
Sanmylly <sanmyllyw@gmail.com>
2023-09-22 00:04:39 -0300

    lint corrections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,15 @@
 import { Link } from 'react-router-dom';
-import drinkIcon from '../../images/drinkIcon.svg';
-import mealIcon from '../../images/mealIcon.svg';
+import drinkIcon from '../images/drinkIcon.svg'
+import mealIcon from '../images/mealIcon.svg'
 
 function Footer() {
   return (
     <footer data-testid="footer">
       <Link to="/drinks">
-        <img src={drinkIcon} alt="Drink Icon" data-testid="drinks-bottom-btn"/>
+        <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
       </Link>
       <Link to="/meals">
-        <img src={mealIcon} alt="Meal Icon" data-testid="meals-bottom-btn"/>
+        <img src={ mealIcon } alt="Meal Icon" data-testid="meals-bottom-btn" />
       </Link>
     </footer>
   );
