703f84c2b9a57f7f45e8e10e828278cc2d44ea25
Sanmylly <sanmyllyw@gmail.com>
2023-09-22 01:17:51 -0300

    16,17 & 18 missing tests

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import drinkIcon from '../images/drinkIcon.svg';
 import mealIcon from '../images/mealIcon.svg';
+import '../styles/footer.css';
 
 function Footer() {
+  const navigate = useNavigate();
   return (
-    <footer data-testid="footer">
-      <Link to="/drinks">
+    <footer data-testid="footer" className="footer">
+      <button onClick={ () => navigate('/drinks') }>
         <img src={ drinkIcon } alt="Drink Icon" data-testid="drinks-bottom-btn" />
-      </Link>
-      <Link to="/meals">
+      </button>
+      <button onClick={ () => navigate('/meals') }>
         <img src={ mealIcon } alt="Meal Icon" data-testid="meals-bottom-btn" />
-      </Link>
+      </button>
     </footer>
   );
 }
