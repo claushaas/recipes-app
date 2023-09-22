471bd0c189bff1598c0917156f436e6d1a8d8203
Sanmylly <sanmyllyw@gmail.com>
2023-09-21 22:57:41 -0300

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,18 @@
+import { Link } from 'react-router-dom';
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+
 function Footer() {
   return (
-    <h1>Footer</h1>
-  )
+    <footer data-testid="footer">
+      <Link to="/drinks">
+        <img src={drinkIcon} alt="Drink Icon" data-testid="drinks-bottom-btn"/>
+      </Link>
+      <Link to="/meals">
+        <img src={mealIcon} alt="Meal Icon" data-testid="meals-bottom-btn"/>
+      </Link>
+    </footer>
+  );
 }
+
+export default Footer;
