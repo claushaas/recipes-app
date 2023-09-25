7a7fd9c90880ace5663b080234dc4b6a92f90bae
Higor Gabriel <higor448.pessoal@gmail.com>
2023-09-25 18:32:53 -0300

    req-21-fix: Correção

--- src/components/CategoryButton.tsx
+++ src/components/CategoryButton.tsx
@@ -29,7 +29,7 @@ function CategoryButton() {
   };
 
   const filterByCategoryDrink = async (category: string) => {
-    const URL = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
+    const URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}`;
     const response = await fetch(URL);
     const data = await response.json();
 
