96d54b002f18bfeaed00d978e75be551882745da
Claus Haas <claus.haas@me.com>
2023-09-27 12:46:50 -0300

    req 34

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,11 +1,11 @@
-import { useSelector } from 'react-redux';
-import { ReduxState } from '../types';
+// import { useSelector } from 'react-redux';
+// import { ReduxState } from '../types';
 
 function Carousel() {
-  const meals = useSelector((state: ReduxState) => state.meals);
-  const drinks = useSelector((state: ReduxState) => state.drinks);
-  console.log(meals);
-  console.log(drinks);
+  // const meals = useSelector((state: ReduxState) => state.meals);
+  // const drinks = useSelector((state: ReduxState) => state.drinks);
+  // console.log(meals);
+  // console.log(drinks);
 
   return (
     <div>Carousel</div>
--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -1,6 +1,40 @@
+import { useSelector } from 'react-redux';
+import { RecipeDetailsType, ReduxState } from '../types';
+
 function FavoriteButton() {
+  const details = useSelector((state: ReduxState) => {
+    return state.recipeDetails.details?.meals?.[0] as RecipeDetailsType
+      || state.recipeDetails.details?.drinks?.[0] as RecipeDetailsType;
+  });
+
+  const favoriteRecipes = JSON.parse(
+    localStorage.getItem('favoriteRecipes') as string,
+  ) || [];
+
+  const handleClick = () => {
+    const newFavoriteRecipe = {
+      id: details.idMeal || details.idDrink,
+      type: details.idMeal ? 'meal' : 'drink',
+      nationality: details.strArea || '',
+      category: details.strCategory || '',
+      alcoholicOrNot: details.strAlcoholic || '',
+      name: details.strMeal || details.strDrink,
+      image: details.strMealThumb || details.strDrinkThumb,
+    };
+
+    localStorage.setItem(
+      'favoriteRecipes',
+      JSON.stringify([...favoriteRecipes, newFavoriteRecipe]),
+    );
+  };
+
   return (
-    <button data-testid="favorite-btn">Favorite</button>
+    <button
+      data-testid="favorite-btn"
+      onClick={ handleClick }
+    >
+      Favorite
+    </button>
   );
 }
 
