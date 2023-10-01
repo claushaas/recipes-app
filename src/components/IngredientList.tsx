817d545fb81d08df3e265dc6cb02bbb9c0b81df5
Claus Haas <claus.haas@me.com>
2023-10-01 11:17:47 -0300

    mais um ajuste

--- src/components/IngredientList.tsx
+++ src/components/IngredientList.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MealDetails } from '../types';
+import '../styles/ingredientList.css';
 
 type IngredientListProps = {
   recipe: MealDetails;
